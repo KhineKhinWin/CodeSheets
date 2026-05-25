ec4baa0327d0c642a5ea507204e1d1290ca62c0a
KhineKhinWin <khinekhinwinkhinekhinwin@gmail.com>
2026-05-25 14:04:45 +0630

    create crud

---
+++ src/main/java/com/cheatsheet/model/CheatSheetCRUD.java
@@ -0,0 +1,15 @@
+package com.cheatsheet.model;
+
+public class CheatSheetCRUD {
+
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+}
